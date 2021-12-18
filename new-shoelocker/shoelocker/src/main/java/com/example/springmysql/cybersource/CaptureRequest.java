74e0068731c97384fac86ca56809b0d8599ca836
Rich Pham <chillpham123@gmail.com>
2021-12-18 02:06:45 -0800

    Add files via upload

---
+++ new-shoelocker/shoelocker/src/main/java/com/example/springmysql/cybersource/CaptureRequest.java
@@ -0,0 +1,12 @@
+package com.example.springmysql.cybersource;
+
+
+public class CaptureRequest extends Payload {
+
+    public String reference ;
+    public String paymentId ;
+    public String transactionAmount ;
+    public String transactionCurrency ;
+
+}
+
